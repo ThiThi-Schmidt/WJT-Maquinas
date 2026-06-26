cf23c0515ceb69ae2b26686ad261a8c87bfeb846
ThiThi-Schmidt <t.schmidt@aluno.senai.br>
2026-06-26 13:48:27 -0300

    cd

--- BackEnd/src/lb/prisma.ts
+++ BackEnd/src/lb/prisma.ts
@@ -1,6 +1,7 @@
 import "dotenv/config";
 import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
-import { PrismaClient } from "../../generated/prisma/client.js";
+import { PrismaClient } from "@prisma/client/extension";
+
 
 if (!process.env.DATABASE_URL) {
   throw new Error("DATABASE_URL não foi definida no arquivo .env");
