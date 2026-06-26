138fd2f6426fd57e3ce95b6121e952b5f4eb72b0
ThiThi-Schmidt <t.schmidt@aluno.senai.br>
2026-06-26 14:00:04 -0300

    ggf

--- BackEnd/src/lb/prisma.ts
+++ BackEnd/src/lb/prisma.ts
@@ -1,6 +1,7 @@
 import "dotenv/config";
 import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
-import { PrismaClient } from "@prisma/client/extension";
+import { PrismaClient } from "../../generated/prisma/client.js";
+
 
 
 if (!process.env.DATABASE_URL) {
