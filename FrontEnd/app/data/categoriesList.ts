98f450f89b3376281abf46d9ecd91e34994aed31
ThiThi-Schmidt <t.schmidt@aluno.senai.br>
2026-06-26 17:08:37 -0300

    ttj

--- FrontEnd/app/data/categoriesList.ts
+++ FrontEnd/app/data/categoriesList.ts
@@ -1,5 +1,6 @@
 export const categoriesList = [
-  { id: 1, name: "Máquinas" },
-  { id: 2, name: "Ferramentas" },
-  { id: 3, name: "Baterias" }
+  { id: 1, name: "Ferramentas" },
+  { id: 2, name: "Maquinas" },
+  { id: 3, name: "Baterias" },
+  { id: 4, name: "Jardinagem" }
 ];
